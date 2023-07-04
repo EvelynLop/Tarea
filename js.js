9bf0a6160bf03d09a0ba9e9783014058674669c9
Evelyn <beatlyn007@gmail.com>
2023-07-04 19:28:24 -0300

    .

--- js.js
+++ js.js
@@ -6,18 +6,44 @@
 //si los parámetros no son correctos, la función debe cortar
 //su ejecución y devolver un mensaje de error
 
-let nombre = prompt('Cual es tu nombre?')
+/*let nombre = prompt('Cual es tu nombre?')
 let añoDeNacimiento = prompt ('En que año naciste?')
-let nacimientoParseado = parseInt(añoDeNacimiento)
 
-const calcularAñosPerro = (nombre, añoDeNacimiento) =>{
+const calcularAñosPerro = (nombre, añoDeNacimiento) => {
     if(typeof nombre !== 'string' || typeof añoDeNacimiento !== 'number'){
     alert('Los datos no son correctos');
-    return;
+    
 }else{
-    edadHumana = 2023 - nacimientoParseado;
+    edadHumana = 2023 - añoDeNacimiento;
     edadDePerro = edadHumana*7;
-    alert(`Hola ${nombre}. Tu tienes ${edadHumana} años y en edad de perro tendrais ${edadDePerro} años`)
-    return;
+    alert(`Hola ${nombre}. Tu tienes ${edadHumana} años y en edad de perro tendrais ${edadDePerro} años`);
 }
 }
+calcularAñosPerro(parseInt(añoDeNacimiento));*/
+
+
+///escribir una función getSquare()
+//que guarde en una variable el cuadrado de un número que reciba 
+//por parametro
+
+/*let numero = prompt('Escribe un numero')
+//let numberParseado = parseInt(number)xxx
+const getSquare = (number) => {
+   let numeroElegido = Math.pow(number,2);
+   alert(`Respuesta: ${number} al cuadrado es ${numeroElegido}`);
+}
+getSquare(parseInt(number));*/
+
+const number = prompt('ecribe un numero')
+
+const getSquare = (numeroElegido) => {
+    let resultado = Math.pow(numeroElegido,2);
+    alert( `Respuesta: ${numeroElegido} al cuadrado es ${resultado}`);
+}
+
+getSquare(parseInt(number));
+
+
+
+
+
